c0b9b30bad76410505b719b04da4ee16b31efa2f
Terence Tong <tmtong@gmail.com>
2025-05-21 11:02:31 +0800

    Wed May 21 11:02:31 AM HKT 2025

--- norgateextractor/allstocks.py
+++ norgateextractor/allstocks.py
@@ -1,6 +1,7 @@
 import logging
 import norgatedata
 import multiprocessing as mp
+import re
 
 def get_all_market_symbols():
     print("Retrieving all symbols from US Equities and US Equities Delisted...")
@@ -11,6 +12,8 @@ def get_all_market_symbols():
     print(f"Retrieved {len(us_equities)} active symbols and {len(us_delisted)} delisted symbols.")
     return us_equities, us_delisted
 
+def get_all_indexs():
+    return ['$DJI', '$NDX', '$NXTQ', '$NGX', '$NDXT', '$NBI', '$RT200', '$RUI', '$RUT', '$RUA', '$RMC', '$RUMIC', '$RSCC', '$OEX', '$SPX', '$MID', '$SML', '$SP1500', '$SP1000', '$SP900', '$SPDAUDP', '$SPESG', '$N1-150', '$N1-200', '$R1-4000', '$R1001-4000', '$R3001-4000', '$R2001-3000', '$SP101-500']
 
 
 
@@ -27,7 +30,9 @@ def download_stock_data(symbol):
         timeseriesformat=timeseriesformat
     )
     dirpath = './data/stock/'
-    pricedata.to_csv(dirpath + symbol  + '.symbol')
+    symbol = re.sub(r'[^a-zA-Z0-9]', '', symbol)
+    filename = dirpath + symbol
+    pricedata.to_csv(filename  + '.symbol')
     return pricedata
 
 
@@ -43,4 +48,5 @@ def multi_download(symbols):
 if __name__ == "__main__":
     active_symbols, delisted_symbols = get_all_market_symbols()
     all_symbols = active_symbols + delisted_symbols
-    multi_download(all_symbols)
+    all_indexs = get_all_indexs()
+    multi_download(all_indexs)
