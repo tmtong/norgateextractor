7f1b73b4ad9b669ccb83b2d173409d22fefbee7c
Terence Tong <tmtong@gmail.com>
2026-05-22 21:53:31 +0800

    Fri May 22 09:53:31 PM HKT 2026

--- norgateextractor/server.py
+++ norgateextractor/server.py
@@ -33,9 +33,10 @@ METRICS_DIR  = os.path.join(MOUNTPOINT, "stockmetrics")
 CACHE_DIR    = os.path.join(MOUNTPOINT, "cache")
 
 DATE_FORMAT  = "%Y-%m-%d"
-START_DATE   = "1998-01-01"
+START_DATE   = "1990-01-01"
 END_DATE     = "2025-01-01"
 INDEX_SYMBOL = "INDEX-SP900"
+# INDEX_SYMBOL = "INDEX-ETF"
 
 os.makedirs(CACHE_DIR, exist_ok=True)
 
